b173da56f2186f20d8ade35b0dbf5a315a5eab23
Mahsa Nurfarhan Hidayat <farhan@budiluhur.sch.id>
2026-05-12 14:46:50 +0700

    feat(wifi): menambahkan fitur untuk me-manage wifi

---
+++ app/Filament/Resources/Wifis/Pages/ListWifis.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Filament\Resources\Wifis\Pages;
+
+use App\Filament\Resources\Wifis\WifiResource;
+use Filament\Actions\CreateAction;
+use Filament\Resources\Pages\ListRecords;
+use Filament\Support\Icons\Heroicon;
+
+class ListWifis extends ListRecords
+{
+    protected static string $resource = WifiResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            CreateAction::make()->icon(Heroicon::PlusCircle)->label("Tambah wifi"),
+        ];
+    }
+}
---
+++ app/Filament/Resources/Wifis/WifiResource.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Filament\Resources\Wifis;
+
+use App\Filament\Resources\Wifis\Pages\CreateWifi;
+use App\Filament\Resources\Wifis\Pages\EditWifi;
+use App\Filament\Resources\Wifis\Pages\ListWifis;
+use App\Filament\Resources\Wifis\Pages\ViewWifi;
+use App\Filament\Resources\Wifis\Schemas\WifiForm;
+use App\Filament\Resources\Wifis\Schemas\WifiInfolist;
+use App\Filament\Resources\Wifis\Tables\WifisTable;
+use App\Models\Wifi;
+use BackedEnum;
+use Filament\Resources\Resource;
+use Filament\Schemas\Schema;
+use Filament\Support\Icons\Heroicon;
+use Filament\Tables\Table;
+use UnitEnum;
+
+class WifiResource extends Resource
+{
+    protected static ?string $model = Wifi::class;
+
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedWifi;
+
+    protected static ?string $modelLabel = 'Wifi';
+
+    protected static ?string $pluralModelLabel = 'Daftar Wifi';
+    protected static UnitEnum|string|null $navigationGroup = 'Jaringan';
+
+    protected static ?string $recordTitleAttribute = 'ssid';
+
+    public static function form(Schema $schema): Schema
+    {
+        return WifiForm::configure($schema);
+    }
+
+    public static function infolist(Schema $schema): Schema
+    {
+        return WifiInfolist::configure($schema);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return WifisTable::configure($table);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getNavigationBadge(): ?string
+    {
+        return (string) static::getModel()::count();
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => ListWifis::route('/'),
+            'create' => CreateWifi::route('/create'),
+            'view' => ViewWifi::route('/{record}'),
+            'edit' => EditWifi::route('/{record}/edit'),
+        ];
+    }
+}
